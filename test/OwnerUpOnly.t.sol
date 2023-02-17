f9730e73ad451a6a4dfe981ef2b9cfdccfb1ae52
Artur Begyan <arturbegyan98@gmail.com>
2023-02-17 00:34:50 +0000

    add remappings

--- test/OwnerUpOnly.t.sol
+++ test/OwnerUpOnly.t.sol
@@ -4,7 +4,7 @@ import "forge-std/Test.sol";
 
 error Unauthorized();
 
-contract OwnerOnly {
+contract OwnerUpOnly {
     address public immutable owner;
     uint256 public count;
 
@@ -20,3 +20,36 @@ contract OwnerOnly {
     }
 }
 
+contract OwnerUpOnlyTest is Test { 
+    OwnerUpOnly upOnly;
+
+    function setUp() public {
+        upOnly = new OwnerUpOnly();
+    }
+
+    function testIncrementAsOwner() public { 
+        assertEq(upOnly.count(), 0);
+        upOnly.increment();
+        assertEq(upOnly.count(), 1);
+    }
+
+    function testFailIncrementAsNotOwner() public {
+        // the prank cheatcode changed our identity to the zero address
+        // for the next call upOnly.increment()
+        /*  
+        note: using testFail is considered an anti-pattern since it does not tell us anything about
+        why upOnly.increment() reverted
+        */
+        vm.prank(address(0)); 
+        upOnly.increment();
+    }
+
+    // replacing testFail with test
+
+    function testIncrementAsNotOwner() public {
+        vm.expectRevert(Unauthorized.selector);
+        vm.prank(address(0));
+        upOnly.increment();
+    }
+
+}
