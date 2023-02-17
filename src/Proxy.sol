e7540e4e1f81e89b1f28c97425def0a9f7c637dc
Artur Begyan <arturbegyan98@gmail.com>
2023-02-17 02:33:47 +0000

    IMarket manager

--- src/Proxy.sol
+++ src/Proxy.sol
@@ -1,15 +1,13 @@
-// inspiration: https://github.com/Synthetixio/synthetix-v3/blob/main/protocol/synthetix/contracts/Proxy.sol 
+// inspiration: https://github.com/Synthetixio/synthetix-v3/blob/main/protocol/synthetix/contracts/Proxy.sol
 //SPDX-License-Identifier: MIT
 pragma solidity >=0.8.13;
 
 // https://www.npmjs.com/package/@synthetixio/core-contracts?activeTab=explore
-import {UUPSProxyWithOwner} from "@synthetixio/core-contracts/contracts/proxy/UUPSProxyWithOwner.sol";
+// import "@synthetixio/core-contracts/contracts/proxy/UUPSProxyWithOwner.sol"; // todo: figure out how to fix this error
 
-
-// contract Proxy is UUPSProxyWithOwner {
-//     // solhint-disable-next-line no-empty-blocks
-//     constructor(
-//         address firstImplementation,
-//         address initialOwner
-//     ) UUPSProxyWithOwner(firstImplementation, initialOwner) {}
-// }
+contract Proxy {
+    // solhint-disable-next-line no-empty-blocks
+    constructor(address firstImplementation, address initialOwner) 
+    // UUPSProxyWithOwner(firstImplementation, initialOwner);
+    {}
+}
