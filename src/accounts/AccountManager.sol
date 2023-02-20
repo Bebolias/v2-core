96b681b6aab9b6143f366d05982a808bba8fc351
Artur Begyan <arturbegyan98@gmail.com>
2023-02-20 23:13:40 +0000

    account manager interface

---
+++ src/accounts/AccountManager.sol
@@ -0,0 +1,10 @@
+// SPDX-License-Identifier: MIT
+pragma solidity >=0.8.13;
+
+import "../interfaces/IAccountManager.sol";
+
+/**
+ * @title Account Manager.
+ * @dev See IAccountManager.
+ */
+contract AccountManager is IAccountManager {}
---
+++ src/interfaces/IAccountManager.sol
@@ -0,0 +1,52 @@
+// SPDX-License-Identifier: MIT
+pragma solidity >=0.8.18;
+
+/**
+ * @title Account Manager Interface.
+ * @notice Manages the system's account token NFT. Every user will need to register an account before being able to interact with the protocol..
+ */
+interface IAccountManager {
+    /**
+     * @notice Emitted when an account token with id `accountId` is minted to `owner`.
+     * @param accountId The id of the account.
+     * @param owner The address that owns the created account.
+     */
+    event AccountCreated(uint128 indexed accountId, address indexed owner);
+
+    /**
+     * @notice Mints an account token with id `requestedAccountId` to `msg.sender`.
+     * @param requestedAccountId The id requested for the account being created. Reverts if id already exists.
+     *
+     * Requirements:
+     *
+     * - `requestedAccountId` must not already be minted.
+     *
+     * Emits a {AccountCreated} event.
+     */
+    function createAccount(uint128 requestedAccountId) external;
+
+    /**
+     * @notice Called by AccountToken to notify the system when the account token is transferred.
+     * @dev Resets user permissions and assigns ownership of the account token to the new holder.
+     * @param to The new holder of the account NFT.
+     * @param accountId The id of the account that was just transferred.
+     *
+     * Requirements:
+     *
+     * - `msg.sender` must be the account token.
+     */
+    function notifyAccountTransfer(address to, uint128 accountId) external;
+
+    /**
+     * @notice Returns the address for the account token used by the manager.
+     * @return accountNftToken The address of the account token.
+     */
+    function getAccountTokenAddress() external view returns (address accountNftToken);
+
+    /**
+     * @notice Returns the address that owns a given account, as recorded by the protocol.
+     * @param accountId The account id whose owner is being retrieved.
+     * @return owner The owner of the given account id.
+     */
+    function getAccountOwner(uint128 accountId) external view returns (address owner);
+}
