5a4c089aa67b683bcb1dd5d398a98c776efaef57
Artur Begyan <arturbegyan98@gmail.com>
2023-02-20 23:26:26 +0000

    ierc20 util interfaces

---
+++ src/interfaces/IAccountToken.sol
@@ -0,0 +1,7 @@
+// SPDX-License-Identifier: MIT
+pragma solidity >=0.8.13;
+
+/**
+ * @title Module with custom NFT logic for the account token.
+ */
+interface IAccountTokenModule {}
---
+++ src/utils/interfaces/INFT.sol
@@ -0,0 +1,7 @@
+// SPDX-License-Identifier: MIT
+pragma solidity >=0.8.13;
+
+/**
+ * @title Module wrapping an ERC721 token implementation.
+ */
+interface INftModule is IERC721Enumerable {}
