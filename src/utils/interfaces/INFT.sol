dac5d13289b4a6bd43782be51d837d89af3c61e9
Artur Begyan <arturbegyan98@gmail.com>
2023-02-20 23:27:36 +0000

    wire up INFT to IERC721Enumerable

--- src/utils/interfaces/INFT.sol
+++ src/utils/interfaces/INFT.sol
@@ -1,7 +1,9 @@
 // SPDX-License-Identifier: MIT
 pragma solidity >=0.8.13;
 
+import "./IERC721Enumerable.sol";
+
 /**
  * @title Module wrapping an ERC721 token implementation.
  */
-interface INftModule is IERC721Enumerable {}
+interface INFT is IERC721Enumerable {}
