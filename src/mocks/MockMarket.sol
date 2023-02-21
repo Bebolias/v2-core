e876271223d2af3b7cab7e389ce625019526a255
Artur Begyan <arturbegyan98@gmail.com>
2023-02-21 23:10:50 +0000

    mock market setup

--- src/mocks/MockMarket.sol
+++ src/mocks/MockMarket.sol
@@ -4,4 +4,33 @@ pragma solidity >=0.8.13;
 import "../interfaces/IMarket.sol";
 import "../interfaces/IMarketManager.sol";
 
-contract MockMarket is IMarket {}
+contract MockMarket is IMarket {
+    address private _proxy;
+    uint128 private _marketId;
+    uint256 private _price;
+
+    function initialize(address proxy, uint128 marketId, uint256 initialPrice) external {
+        _proxy = proxy;
+        _marketId = marketId;
+        _price = initialPrice;
+    }
+
+    function name(uint128) external pure override returns (string memory) {
+        return "MockMarket";
+    }
+
+    function setPrice(uint256 newPrice) external {
+        _price = newPrice;
+    }
+
+    function price() external view returns (uint256) {
+        return _price;
+    }
+
+    /**
+     * @dev See {IERC165-supportsInterface}.
+     */
+    function supportsInterface(bytes4 interfaceId) public view virtual override(IERC165) returns (bool) {
+        return interfaceId == type(IMarket).interfaceId || interfaceId == this.supportsInterface.selector;
+    }
+}
