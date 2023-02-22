175b4b01aa8e89ec184caff936742034383cf0ff
Artur Begyan <arturbegyan98@gmail.com>
2023-02-22 00:24:07 +0000

    collateral engine skeleton

---
+++ src/margin-engine/CollateralEngine.sol
@@ -0,0 +1,10 @@
+//SPDX-License-Identifier: MIT
+pragma solidity >=0.8.13;
+
+import "../accounts/storage/Account.sol";
+
+/**
+ * @title Module for managing user collateral.
+ * @dev See ICollateralEngine.
+ */
+contract CollateralEngine is ICollateralEngine {}
