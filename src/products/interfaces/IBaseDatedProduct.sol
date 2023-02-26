93ff67dae20cd84f7b17eee78745040ea9de476c
Artur Begyan <arturbegyan98@gmail.com>
2023-02-26 20:52:44 +0000

    settle interface base dated product

--- src/products/interfaces/IBaseDatedProduct.sol
+++ src/products/interfaces/IBaseDatedProduct.sol
@@ -4,4 +4,15 @@ pragma solidity >=0.8.13;
 import "../../interfaces/IProduct.sol";
 
 /// @title Interface of a base dated product
-interface IBaseDatedProduct is IProduct {}
+interface IBaseDatedProduct is IProduct {
+    // process taker and maker orders & single pool
+    // settle
+
+    /**
+     * @notice Returns the address that owns a given account, as recorded by the protocol.
+     * @param accountId Id of the account that wants to settle
+     * @param marketId Id of the market in which the account wants to settle (e.g. 1 for aUSDC lend)
+     * @param maturityTimestamp Maturity timestamp of the market in which the account wants to settle
+     */
+    function settle(uint128 accountId, uint128 marketId, uint256 maturityTimestamp) external view;
+}
