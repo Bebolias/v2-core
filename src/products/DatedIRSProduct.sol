e7d4db13097cef4a96b1b42912ab86b421a8c6e1
Artur Begyan <arturbegyan98@gmail.com>
2023-02-26 23:10:43 +0000

    update position flow in dated irs portfolio

--- src/products/DatedIRSProduct.sol
+++ src/products/DatedIRSProduct.sol
@@ -1,1 +1,80 @@
+//SPDX-License-Identifier: MIT
+pragma solidity >=0.8.13;
 
+import "./interfaces/IBaseDatedProduct.sol";
+import "../accounts/storage/Account.sol";
+import "./storage/DatedIRSPortfolio.sol"
+
+// todo: no need for base for no since not doing dated futures in the near future
+
+/**
+ * @title BaseDatedProduct abstract contract
+ * @dev See IBaseDatedProduct
+ */
+
+contract DatedIRSProduct is IBaseDatedProduct {
+    using Account for Account.Data;
+
+    /**
+     * @inheritdoc IBaseDatedProduct
+     */
+    function initiateTakerOrder(uint128 accountId, uint128 marketId, uint256 maturityTimestamp) external override {
+        // check if account exists
+        // check if market id is valid + check there is an active pool with maturityTimestamp requested
+        // note: portfolio and account id are the same
+        DatedIRSPortfolio.Data storage portfolio = DatedIRSPortfolio.load(accountId);
+        (int256 executedBaseAmount, int256 executedQuoteAmount) = pool.executeTakerOrder(marketId, maturityTimestamp);
+        // the storage position object can execute this
+    }
+    /**
+     * @inheritdoc IBaseDatedProduct
+     */
+
+    function settle(uint128 accountId, uint128 marketId, uint256 maturityTimestamp) external override {}
+
+    /**
+     * @inheritdoc IBaseDatedProduct
+     */
+    function initiateMakerOrder(
+        uint128 accountId,
+        uint128 marketId,
+        uint256 maturityTimestamp,
+        uint256 priceLower,
+        uint256 priceUpper
+    ) external override {}
+
+    /**
+     * @inheritdoc IProduct
+     */
+    function name(uint128 productId) external pure override returns (string memory) {
+        // todo: make this virtual
+        return "Dated Product";
+    }
+
+    /**
+     * @inheritdoc IProduct
+     */
+    function getAccountUnrealizedPnL(uint128 accountId) external view override returns (int256 unrealizedPnL) {}
+
+    /**
+     * @inheritdoc IProduct
+     */
+    function getAccountAnnualizedExposures(uint128 accountId)
+        external
+        view
+        override
+        returns (Account.Exposure[] memory exposures)
+    {}
+
+    /**
+     * @inheritdoc IProduct
+     */
+    function closeAccount(uint128 accountId) external override {}
+
+    /**
+     * @inheritdoc IERC165
+     */
+    function supportsInterface(bytes4 interfaceId) public view virtual override(IERC165) returns (bool) {
+        return interfaceId == type(IProduct).interfaceId || interfaceId == this.supportsInterface.selector;
+    }
+}
---
+++ src/products/storage/DatedIRSPortfolio.sol
@@ -0,0 +1,65 @@
+// SPDX-License-Identifier: MIT
+pragma solidity >=0.8.13;
+
+import "../../utils/helpers/SetUtil.sol";
+
+/**
+ * @title Object for tracking dated positions, e.g. locked dated interest rate swap contracts
+ */
+library DatedIRSPortfolio {
+    struct Data {
+        /**
+         * @dev Numeric identifier for the account that owns the portfolio.
+         * @dev Since a given account can only own a single portfolio in a given dated product
+         * the id of the portfolio is the same as the id of the account
+         * @dev There cannot be an account and hence dated portfolio with id zero
+         */
+        uint128 accountId;
+        /**
+         * @dev marketId (e.g. aUSDC lend) --> maturityTimestamp (e.g. 31st Dec 2023) --> DatedIRSPosition object with filled balances
+         */
+        mapping(uint128 => mapping(uint256 => DatedIRSPosition.Data)) positions;
+        /**
+         * @dev Ids of all the markets in which the account has active positions
+         * todo: needs logic to mark active markets
+         */
+        SetUtil.UintSet activeMarkets;
+        /**
+         * @dev marketId (e.g. aUSDC lend) -> activeMaturities (e.g. 31st Dec 2023)
+         */
+        mapping(uint128 => SetUtil.UintSet) activeMaturitiesPerMarket;
+    }
+
+    /**
+     * @dev Returns the portfolio stored at the specified portfolio  id.
+     */
+    function load(uint128 id) internal pure returns (Data storage portfolio) {
+        bytes32 s = keccak256(abi.encode("xyz.voltz.DatedIRSPortfolio", id));
+        assembly {
+            account.slot := s
+        }
+    }
+
+    /**
+     * @dev Creates a portfolio for a given id, the id of the portfolio and the account that owns it are the same
+     */
+    function create(uint128 id) internal returns (Data storage portfolio) {
+        portfolio = load(id);
+        // note, the portfolio id is the same as the account id that owns this portfolio
+        portfolio.accountId = id;
+    }
+
+    /**
+     * @dev create, edit or close an irs position for a given marketId (e.g. aUSDC lend) and maturityTimestamp (e.g. 31st Dec 2023)
+     */
+    function updatePosition(
+        Data storage self,
+        uint128 marketId,
+        uint256 maturityTimestamp,
+        int256 baseDelta,
+        int256 quoteDelta
+    ) internal {
+        DatedIRSPosition.Data storage position = self.positions[marketId][maturityTimestamp];
+        position.update(baseDelta, quoteDelta);
+    }
+}
