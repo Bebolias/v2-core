0d7ae085eb94ec758d193ddb05180cc2828d99fc
Artur Begyan <arturbegyan98@gmail.com>
2023-02-26 22:45:30 +0000

    dated portfolio and a dated position

--- src/products/storage/DatedPortfolio.sol
+++ src/products/storage/DatedPortfolio.sol
@@ -1,6 +1,8 @@
 // SPDX-License-Identifier: MIT
 pragma solidity >=0.8.13;
 
+import "../../utils/helpers/SetUtil.sol";
+
 /**
  * @title Object for tracking dated positions, e.g. locked dated interest rate swap contracts
  */
@@ -14,8 +16,17 @@ library DatedPortfolio {
          */
         uint128 id;
         /**
-         * @dev marketId (e.g. aUSDC lend) --> maturityTimestamp (e.g. 31st Dec 2023) --> DatedPosition object with filled balacnes
+         * @dev marketId (e.g. aUSDC lend) --> maturityTimestamp (e.g. 31st Dec 2023) --> DatedPosition object with filled balances
          */
-        mapping(uint128 => mapping(uint256 => DatedPosition.Data)) datedPositions;
+        mapping(uint128 => mapping(uint256 => DatedPosition.Data)) positions;
+        /**
+         * @dev Ids of all the markets in which the account has active positions
+         * todo: needs logic to mark active markets
+         */
+        SetUtil.UintSet activeMarkets;
+        /**
+         * @dev marketId (e.g. aUSDC lend) -> activeMaturities (e.g. 31st Dec 2023)
+         */
+        mapping(uint128 => SetUtil.UintSet) activeMaturitiesPerMarket;
     }
 }
