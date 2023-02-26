60b597c4d8583676b788169190e4dfa04f097bf1
Artur Begyan <arturbegyan98@gmail.com>
2023-02-26 20:44:32 +0000

    introduce IBaseDatedProduct

---
+++ src/products/interfaces/IBaseDatedProduct.sol
@@ -0,0 +1,7 @@
+//SPDX-License-Identifier: MIT
+pragma solidity >=0.8.13;
+
+import "../../interfaces/IProduct.sol";
+
+/// @title Interface of a base dated product
+interface IBaseDatedProduct is IProduct {}
