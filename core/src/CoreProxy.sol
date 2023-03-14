987e84ad5effb1539c383c790659866cf35970d9
Artur Begyan <arturbegyan98@gmail.com>
2023-03-14 00:22:32 +0000

    chore: remove todo

--- core/src/CoreProxy.sol
+++ core/src/CoreProxy.sol
@@ -5,10 +5,10 @@ import "./utils/contracts/proxy/UUPSProxyWithOwner.sol";
 
 /**
  * Voltz V2 Core Proxy Contract
- *
- * todo: can we do smth like this? https://usecannon.com/packages/synthetix to interact with this protocol
  */
 contract CoreProxy is UUPSProxyWithOwner {
     // solhint-disable-next-line no-empty-blocks
-    constructor(address firstImplementation, address initialOwner) UUPSProxyWithOwner(firstImplementation, initialOwner) { }
+    constructor(address firstImplementation, address initialOwner)
+        UUPSProxyWithOwner(firstImplementation, initialOwner)
+    {}
 }
