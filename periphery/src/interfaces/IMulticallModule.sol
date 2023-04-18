926fc48d80db650f8b3db1affd634dfb0b9e5a91
Artur Begyan <arturbegyan98@gmail.com>
2023-04-18 21:34:51 +0100

    feat: multicall module does not have storage

--- periphery/src/interfaces/IMulticallModule.sol
+++ periphery/src/interfaces/IMulticallModule.sol
@@ -5,7 +5,6 @@ pragma solidity >=0.8.13;
  * @title Module that enables calling multiple router proxies and methods of the voltz v2 ecosystem in a single transaction
  */
 interface IMulticallModule {
+    // todo: natspec
     function multiCall(address[] calldata targets, bytes[] calldata data) external payable returns (bytes[] memory);
 }
-
-// https://github.com/Uniswap/v3-periphery/blob/6cce88e63e176af1ddb6cc56e029110289622317/contracts/base/Multicall.sol#L14
