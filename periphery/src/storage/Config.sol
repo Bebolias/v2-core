11cfabf092c12e70cb5cbffa3b2cc4c66de06921
Artur Begyan <arturbegyan98@gmail.com>
2023-04-20 20:40:51 +0100

    feat: load the periphery configuration object

--- periphery/src/storage/Config.sol
+++ periphery/src/storage/Config.sol
@@ -18,16 +18,27 @@ library Config {
         address VOLTZ_V2_DATED_IRS_VAMM_PROXY;
     }
 
-    // /**
-    //  * @dev Configures the periphery
-    //  * @param config The Config object with all the settings for the Periphery
-    //  */
+    /**
+     * @dev Configures the periphery
+     * @param config The Config object with all the settings for the Periphery
+     */
     function set(Data memory config) internal {
-        Data storage storedConfig = load(config.tokenAddress);
+        Data storage storedConfig = load();
         storedConfig.WETH9 = config.WETH9;
         storedConfig.PERMIT2 = config.PERMIT2;
         storedConfig.VOLTZ_V2_CORE_PROXY = config.VOLTZ_V2_CORE_PROXY;
         storedConfig.VOLTZ_V2_DATED_IRS_PROXY = config.VOLTZ_V2_DATED_IRS_PROXY;
         storedConfig.VOLTZ_V2_DATED_IRS_VAMM_PROXY = config.VOLTZ_V2_DATED_IRS_VAMM_PROXY;
+    }
+
+    /**
+     * @dev Loads the periphery configuration
+     * @return config The periphery configuration object.
+     */
+    function load() internal pure returns (Data storage config) {
+        bytes32 s = keccak256(abi.encode("xyz.voltz.PeripheryConfiguration"));
+        assembly {
+            config.slot := s
+        }
     }
 }
