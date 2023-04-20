066f31dd4643322ba0588430d8efb70cdc54a8ae
Artur Begyan <arturbegyan98@gmail.com>
2023-04-20 20:38:25 +0100

    feat: periphery configuration

---
+++ periphery/src/storage/Config.sol
@@ -0,0 +1,33 @@
+// SPDX-License-Identifier: MIT
+pragma solidity >=0.8.13;
+
+import "../interfaces/external/IWETH9.sol";
+
+/**
+ * @title Config
+ */
+library Config {
+    struct Data {
+        /// @dev WETH9 address
+        IWETH9 WETH9;
+        /// @dev Permit2 address
+        IAllowanceTransfer PERMIT2;
+        /// @dev Voltz V2 core proxy address
+        address VOLTZ_V2_CORE_PROXY;
+        address VOLTZ_V2_DATED_IRS_PROXY;
+        address VOLTZ_V2_DATED_IRS_VAMM_PROXY;
+    }
+
+    // /**
+    //  * @dev Configures the periphery
+    //  * @param config The Config object with all the settings for the Periphery
+    //  */
+    function set(Data memory config) internal {
+        Data storage storedConfig = load(config.tokenAddress);
+        storedConfig.WETH9 = config.WETH9;
+        storedConfig.PERMIT2 = config.PERMIT2;
+        storedConfig.VOLTZ_V2_CORE_PROXY = config.VOLTZ_V2_CORE_PROXY;
+        storedConfig.VOLTZ_V2_DATED_IRS_PROXY = config.VOLTZ_V2_DATED_IRS_PROXY;
+        storedConfig.VOLTZ_V2_DATED_IRS_VAMM_PROXY = config.VOLTZ_V2_DATED_IRS_VAMM_PROXY;
+    }
+}
