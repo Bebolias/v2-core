651313c0f38319a04c302b285e88ba817b31d05c
Artur Begyan <arturbegyan98@gmail.com>
2023-04-21 21:32:30 +0100

    feat: swap and settle library implementation

--- periphery/src/libraries/V2DatedIRS.sol
+++ periphery/src/libraries/V2DatedIRS.sol
@@ -1,6 +1,9 @@
 // SPDX-License-Identifier: MIT
 pragma solidity >=0.8.13;
 
+import "@voltz-protocol/products-dated-irs/src/interfaces/IProductIRSModule.sol";
+import "../storage/Config.sol";
+
 /**
  * @title Performs swaps and settements on top of the v2 dated irs instrument
  */
@@ -9,7 +12,12 @@ library V2DatedIRS {
     function swap(uint128 accountId, uint128 marketId, uint32 maturityTimestamp, int256 baseAmount)
         internal
         returns (int256 executedBaseAmount, int256 executedQuoteAmount)
-    {}
+    {
+        (executedBaseAmount, executedQuoteAmount) = IProductIRSModule(Config.load().VOLTZ_V2_DATED_IRS_PROXY)
+            .initiateTakerOrder(accountId, marketId, maturityTimestamp, baseAmount);
+    }
 
-    function settle(uint128 accountId, uint128 marketId, uint32 maturityTimestamp) internal {}
+    function settle(uint128 accountId, uint128 marketId, uint32 maturityTimestamp) internal {
+        IProductIRSModule(Config.load().VOLTZ_V2_DATED_IRS_PROXY).settle(accountId, marketID, maturityTimestamp);
+    }
 }
