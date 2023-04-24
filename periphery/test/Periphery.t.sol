d21b74299f2074ee36cf7c4bc4e0dfab77759c02
Artur Begyan <arturbegyan98@gmail.com>
2023-04-24 17:46:44 +0100

    feat: granular testing

--- periphery/test/Periphery.t.sol
+++ periphery/test/Periphery.t.sol
@@ -3,11 +3,16 @@ pragma solidity >=0.8.13;
 
 import "forge-std/Test.sol";
 import "../src/modules/ExecutionModule.sol";
+import "./mock/MockERC20.sol";
 
-contract UniversalRouterTest is Test {
+contract PeripheryTest is Test {
     address constant RECIPIENT = address(10);
     uint256 constant AMOUNT = 10 ** 18;
 
     ExecutionModule executionModule;
     MockERC20 erc20;
+
+    function setUp() public {
+        // executionModule = ExecutionModule()
+    }
 }
