dd52d25a4032d1b4d22b2d2b89df685ad5977c38
Artur Begyan <arturbegyan98@gmail.com>
2023-04-24 18:05:06 +0100

    feat: exposed payments setup

--- periphery/test/libraries/Payments.t.sol
+++ periphery/test/libraries/Payments.t.sol
@@ -1,3 +1,22 @@
-// SPDX-License-Identifier: MIT
+//SPDX-License-Identifier: MIT
 pragma solidity >=0.8.13;
 
+import "forge-std/Test.sol";
+import "../../src/libraries/Payments.sol";
+
+contract ExposedPayments {
+    // exposed functions
+    function pay(address token, address recipient, uint256 value) external {
+        Payments.pay(token, recipient, value);
+    }
+
+    function wrapETH(address recipient, uint256 amount) external {
+        Payments.wrapETH(recpient, amount);
+    }
+
+    function unwrapWETH9(address recipient, uint256 amountMinimum) external {
+        Payments.unwrapWETH9(recipient, amountMinimum);
+    }
+}
+
+contract PaymentsTest is Test {}
