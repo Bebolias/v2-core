0c803d40cba564534b7f809537c15b40094e9139
Artur Begyan <arturbegyan98@gmail.com>
2023-04-25 12:01:27 +0100

    feat: modify map function for addresses

--- periphery/src/libraries/Dispatcher.sol
+++ periphery/src/libraries/Dispatcher.sol
@@ -115,7 +115,9 @@ library Dispatcher {
     /// @return output The resultant recipient for the command
     function map(address recipient) internal view returns (address) {
         if (recipient == Constants.MSG_SENDER) {
-            return lockedBy;
+            // todo: check the purpose of the locked flow:
+            // https://github.com/Uniswap/universal-router/blob/a88bc6e15af738b61d7bee8feb7df8d2a6e26347/contracts/base/LockAndMsgSender.sol#L26
+            return msg.sender;
         } else if (recipient == Constants.ADDRESS_THIS) {
             return address(this);
         } else {
