231edf93355f5189e410d3555fc3b78085a1a492
Artur Begyan <arturbegyan98@gmail.com>
2023-04-27 10:29:17 +0100

    eth unwrap test

--- periphery/test/libraries/Payments.t.sol
+++ periphery/test/libraries/Payments.t.sol
@@ -7,6 +7,7 @@ import "../../src/libraries/Constants.sol";
 import "../../src/storage/Config.sol";
 import "../../src/interfaces/external/IAllowanceTransfer.sol";
 import "@voltz-protocol/util-contracts/src/interfaces/IERC20.sol";
+import "solmate/src/utils/SafeTransferLib.sol";
 
 contract ExposedPayments {
     function setUp(Config.Data memory config) external {
@@ -76,5 +77,17 @@ contract PaymentsTest is Test {
         vm.expectCall(address(weth9), 2 ether, abi.encodeWithSelector(IWETH9.deposit.selector));
         exposedPayments.wrapETH(address(2), Constants.CONTRACT_BALANCE);
     }
- 
+
+    function testUnwrapWETH9() public {
+        vm.deal(address(exposedPayments), 2 ether);
+        IWETH9 weth9 = IWETH9(address(1));
+        vm.mockCall(address(weth9), 0, abi.encodeWithSelector(IERC20.balanceOf.selector), abi.encode(2 ether));
+        vm.mockCall(address(weth9), abi.encodeWithSelector(IWETH9.withdraw.selector, 2 ether), abi.encode(0));
+        vm.mockCall(
+            address(exposedPayments), 1 ether, abi.encodeWithSelector(exposedPayments.pay.selector), abi.encode(0)
+        );
+
+        vm.expectCall(address(weth9), 0, abi.encodeWithSelector(IERC20.balanceOf.selector));
+        exposedPayments.unwrapWETH9(address(3), 1 ether);
+    }
 }
