d18a2fe016c683ec8a345e1094fc0cd62008fbc7
Alexandru-Vlad Niculae <43644109+avniculae@users.noreply.github.com>
2023-06-12 12:31:17 +0300

    Feat/pre testnet deployment (#70)
    * feat: update cannon files
    
    * feat: update license notice in source files

--- products/dated-irs/src/interfaces/IRateOracleModule.sol
+++ products/dated-irs/src/interfaces/IRateOracleModule.sol
@@ -1,4 +1,10 @@
-// https://github.com/Voltz-Protocol/v2-core/blob/main/products/dated-irs/LICENSE
+/*
+Licensed under the Voltz v2 License (the "License"); you 
+may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+https://github.com/Voltz-Protocol/v2-core/blob/main/products/dated-irs/LICENSE
+*/
 pragma solidity >=0.8.19;
 
 import { UD60x18 } from "@prb/math/UD60x18.sol";
