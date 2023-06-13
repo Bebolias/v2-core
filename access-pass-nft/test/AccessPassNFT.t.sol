53006ad2dbed1128f9d5a81b7c1da08983c18823
Ioana Roceanu <ioanaroceanu@gmail.com>
2023-06-13 21:19:52 +0100

    fix: access pass test fix

--- access-pass-nft/test/AccessPassNFT.t.sol
+++ access-pass-nft/test/AccessPassNFT.t.sol
@@ -3,17 +3,22 @@ pragma solidity >=0.8.19;
 import "forge-std/Test.sol";
 import "../src/AccessPassNFT.sol";
 import "oz/utils/cryptography/MerkleProof.sol";
+import "oz/token/ERC721/IERC721Receiver.sol";
 
-
-
-contract AccessPassNFTTest is Test {
+contract AccessPassNFTTest is Test, IERC721Receiver {
 
     AccessPassNFT internal accessPassNFT;
     string internal constant ACCESS_PASS_NFT_NAME = "AccessPassNFT";
     string internal constant ACCESS_PASS_NFT_SYMBOL = "APNFT";
-    bytes32 internal constant MERKLE_ROOT = "0x1234";
+    bytes32 internal constant MERKLE_ROOT = 0xf1197aaf943f940ff63c7c8c929a35decf1473ff6cdaac3b88b9c8db3aa3e2a9;
     string internal constant BASE_METADATA_URI = "ipfs://QmWKUhiBh7efyvRDDfeyDXKh9KrzchEGCJAVMcTqZezSPg/";
-    uint256 internal constant NUMBER_OF_ACCESS_PASSES = 5;
+    uint256 internal constant NUMBER_OF_ACCESS_PASSES = 3;
+
+    function onERC721Received(address operator, address from, uint256 tokenId, bytes memory data)
+        external
+        returns (bytes4) {
+        return IERC721Receiver.onERC721Received.selector;
+    }
 
     function setUp() public {
         accessPassNFT = new AccessPassNFT(ACCESS_PASS_NFT_NAME, ACCESS_PASS_NFT_SYMBOL);
@@ -42,8 +47,25 @@ contract AccessPassNFTTest is Test {
         accessPassNFT.addNewRoot(rootInfo);
 
         bytes32[] memory proof = new bytes32[](1);
+        proof[0] = 0x2a5bb61d4b6540294819af4b6a2b302e0fcb2b698020f535cd8182b0a910da9f;
 
         accessPassNFT.redeem(address(this), NUMBER_OF_ACCESS_PASSES, proof, MERKLE_ROOT);
+
+    }
+
+    function testSuccessfulRedeemEOAAccount() public {
+
+        AccessPassNFT.RootInfo memory rootInfo = AccessPassNFT.RootInfo({
+            merkleRoot: MERKLE_ROOT,
+            baseMetadataURI: BASE_METADATA_URI
+        });
+
+        accessPassNFT.addNewRoot(rootInfo);
+
+        bytes32[] memory proof = new bytes32[](1);
+        proof[0] = 0xa57c39cbcd238da0214df3df9ed8814d268c19f6440c6cfdd5fe9ae1251055c3;
+
+        accessPassNFT.redeem(address(1), 1, proof, MERKLE_ROOT);
 
     }
 
